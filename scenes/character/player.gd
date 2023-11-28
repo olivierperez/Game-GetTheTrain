366f541b7031c7d278717402b627d8ddb3e1a128
Olivier PEREZ <olivier@olivierperez.fr>
2023-11-28 23:56:39 +0100

    Make the Player jump

--- scenes/character/player.gd
+++ scenes/character/player.gd
@@ -2,8 +2,11 @@ extends CharacterBody2D
 
 class_name PlayablePlayer
 
-
 @export var speed = 250
+@export var gravity = 40.0
+@export var jump_force = 10
+
+var state: State = State.IDLE
 var direction: Vector2 = Vector2.ZERO :
 	get:
 		return direction
@@ -15,17 +18,65 @@ var direction: Vector2 = Vector2.ZERO :
 var facing: Vector2 = Vector2.DOWN
 var disabled: bool = false
 
+@export var vertical_velocity: float = 0
+@export var vertical_position: float = 0
 
-func _physics_process(__):
+enum State { IDLE, RUNNING, JUMPING }
+
+func _physics_process(delta):
 	if disabled:
 		return
 
-	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * speed
+	if Input.is_action_just_pressed("jump") && state != State.JUMPING:
+		_start_jumping()
+
+	match(state):
+		State.IDLE:
+			_process_idle(delta)
+		State.RUNNING:
+			_process_running(delta)
+		State.JUMPING:
+			_process_jumping(delta)
+
 	move_and_slide()
 
 	if get_real_velocity() == Vector2.ZERO:
 		direction = Vector2.ZERO
+
+
+func _start_jumping():
+	vertical_velocity = -jump_force
+	state = State.JUMPING
+	_update_animation()
+
+
+func _process_idle(__):
+	_process_direction_change()
+
+
+func _process_running(__):
+	_process_direction_change()
+
+
+func _process_jumping(delta):
+	vertical_velocity += gravity * delta
+	vertical_position += vertical_velocity
+	var jump_percent = vertical_position / -70
+	$AnimatedSprite2D.scale = Vector2(1 + 0.3 * jump_percent, 1 + 0.3 * jump_percent)
+
+	if vertical_position >= 0:
+		vertical_velocity = 0
+		vertical_position = 0
+		$AnimatedSprite2D.scale = Vector2(1, 1)
+		if direction != Vector2.ZERO:
+			state = State.RUNNING
+		else:
+			state = State.IDLE
+
+
+func _process_direction_change():
+	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * speed
 
 
 func disable():
@@ -39,8 +90,13 @@ func enable():
 
 func _update_animation():
 	var direction_name = _get_direction_name()
+
 	if (direction_name == ""):
 		$AnimatedSprite2D.stop()
+	elif state == State.JUMPING:
+		$AnimatedSprite2D.animation = "Move" + direction_name
+		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.frame = 1
 	else:
 		$AnimatedSprite2D.animation = "Move" + direction_name
 		$AnimatedSprite2D.play()
