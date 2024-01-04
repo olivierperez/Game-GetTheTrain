d607cc8891a883a16af51c9bfef27563f8ceecb8
Olivier PEREZ <olivier@olivierperez.fr>
2024-01-04 22:06:40 +0100

    Make train arriving on level 002

--- actor/object/train.gd
+++ actor/object/train.gd
@@ -3,29 +3,47 @@ class_name Train
 
 signal on_locomotive_entered
 signal on_wagons_entered
+signal on_train_arrived
 
 @export var leaving_destination: Node2D
+@export var arriving_destination: Node2D
 @export var speed: int = 50
 
 var is_leaving: bool = false
+var is_arriving: bool = false
 
 
 func _physics_process(delta):
-	if is_leaving:
+	if is_arriving:
+		position = position.move_toward(arriving_destination.position, delta * speed)
+		var distance_to_arrival = (position - arriving_destination.position).length()
+		if (distance_to_arrival < 1):
+			_on_arrival()
+	elif is_leaving:
 		position = position.move_toward(leaving_destination.position, delta * speed)
 
 
 func _on_locomotive_area_body_entered(body):
-	print("loco entered", body)
 	if body is PlayablePlayer:
 		on_locomotive_entered.emit()
 
 
 func _on_wagons_area_body_entered(body):
-	print("wagons entered", body)
 	if body is PlayablePlayer:
 		on_wagons_entered.emit()
+
+
+func arrive():
+	$SmokeParticles.emitting = true
+	is_arriving = true
+
 
 func leave():
 	$SmokeParticles.emitting = true
 	is_leaving = true
+
+
+func _on_arrival():
+	is_arriving = false
+	$SmokeParticles.emitting = false
+	on_train_arrived.emit()
--- scenes/levels/level_002/level_002.gd
+++ scenes/levels/level_002/level_002.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
+@onready var player: PlayablePlayer = %Player
+@onready var train: Train = %Train
+@onready var machine_hs = %MachineHS
+@onready var ticket_audio = %TicketAudio
+
 
 var has_ticket: bool = false
 
 
 func _ready():
+	train.arrive()
+
+
+func _on_train_arrived():
 	DialogueManager.show_dialogue_balloon(
 		load("res://scenes/levels/level_002/level_002.dialogue"),
 		"start"
@@ -12,21 +21,23 @@ func _ready():
 
 
 func appear():
-	%Player.visible = true
+	player.visible = true
+	player.enable()
+
 
 func buy_ticket():
 	has_ticket = true
-	%TicketAudio.play(0.5)
+	ticket_audio.play(0.5)
 
 
 func play_machine_hs():
-	%MachineHS.play()
+	machine_hs.play()
 
 
 func train_departure():
-	%Train.leave()
-	%Player.visible = false
-	%Player.disable()
+	train.leave()
+	player.visible = false
+	player.disable()
 
 
 func finish():
